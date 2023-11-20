f444140d47ff6312dfa473f70311c02f110e86cf
julianourias <dev.urias@gmail.com>
2023-11-20 12:36:31 -0300

    Class despesas

--- lib/screens/listagem.dart
+++ lib/screens/listagem.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class FirebaseListScreen extends StatefulWidget {
   @override
   _FirebaseListScreenState createState() => _FirebaseListScreenState();
+}
+
+class Despesa {
+  String nome;
+  String descricao;
+  double valor;
+  String data;
+
+  Despesa({
+    required this.nome,
+    required this.descricao,
+    required this.valor,
+    required this.data,
+  });
 }
 
 class _FirebaseListScreenState extends State<FirebaseListScreen> {
@@ -17,16 +32,26 @@ class _FirebaseListScreenState extends State<FirebaseListScreen> {
         stream: FirebaseFirestore.instance.collection('custo').snapshots(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
+            List<Despesa> despesas = snapshot.data!.docs.map((doc) {
+              return Despesa(
+                nome: doc['nome'],
+                descricao: doc['descrição'],
+                valor: doc['valor'].toDouble(),
+                data: doc['data'],
+              );
+            }).toList();
+
             return ListView.builder(
-              itemCount: snapshot.data?.docs.length,
+              itemCount: despesas.length,
               itemBuilder: (context, index) {
-                DocumentSnapshot document = snapshot.data!.docs[index];
-                // Customize the UI for each item in the list
+                Despesa despesa = despesas[index];
+                final formattedValor =
+                    NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$')
+                        .format(despesa.valor);
                 return ListTile(
-                  title: Text(document['nome']),
-                  subtitle:
-                      Text(document['descrição'] + " - " + document['data']),
-                  trailing: Text(document['valor'].toString()),
+                  title: Text(despesa.nome),
+                  subtitle: Text(despesa.descricao + " - " + despesa.data),
+                  trailing: Text(formattedValor),
                 );
               },
             );
